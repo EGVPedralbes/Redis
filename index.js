c238452a148d6e70fa5cae42bd0647765546ec80
Eric Gómez Vilà <105845066+EGVPedralbes@users.noreply.github.com>
2024-02-26 17:58:45 +0100

    NewWork

--- index.js
+++ index.js
@@ -1,42 +1,28 @@
 const redis = require('redis');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express()
 const port = 3000
 
 const client = redis.createClient(6379);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
-setResponse = (username, joke) => {
-    return `<h2>El chiste de ${username} es: ${joke}</h2>`;
-}
+app.post('/submit', (req, res) => {
+    const joke = req.body.joke;
+    const username = req.body.username;
+    if (client.get(joke) == null) {
+        client.setex(username, 60, JSON.stringify(joke))
+        res.send(joke);
 
-function checkCache(req, res, next) {
-    const { username } = req.params;
-    console.log(username);
-    client.get(username, (err, data) => {
-        if (data !== null) {
-            res.send(setResponse(username, data));
-        } else {
-            console.log('no data');
-            next();
-        }
-    });
-}
-
-async function getJoke(req, res, next) {
-    try {
-        const { username } = req.params;
-        const response = await fetch(`https://api.chucknorris.io/jokes/random`);
-        const data = await response.json();
-        res.send(setResponse(username, JSON.stringify(data.value)));
-        console.log(data.value);
-        client.setex(username, 60, JSON.stringify(data.value));
-    } catch (err) {
-        console.log(err);
+    } else {
+        res.send('Tenias el chiste' + joke + 'en cache');
     }
-}
 
-app.get('/joke/:username', checkCache, getJoke);
+
+
+});
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
